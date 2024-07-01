44a9cb3b163a7405962f1c6add656d523b61c83b
Ashish Singh Mahar <asmaharg1139@gmail.com>
2024-07-01 15:21:37 +0530

    Update git1.py

--- PythonTution/git1.py
+++ PythonTution/git1.py
@@ -1,2 +1,90 @@
-print("Hello World!")
-print("hello I am Ashish")
+x = int(input("Enter the value..."))
+for i in range(1,x+1):
+    for j in range(1,x+1):
+        if(i==1 or i==x or j==1 or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(i==1 or i==x or j==1):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(i==x or j==1 or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x-3):
+        if(j==1):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(i==1 or i==x or j==1):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(i==x or j==1 or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    print()
+for i in range(1,x+1):
+    for j in range(1,x+1):
+        if( j==1 or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(i==x or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(j==1 or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x-3):
+        if(j==1):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(i==x or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    for j in range(1,x+1):
+        if(j==1 or j==x):
+            print("x",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(1,x-2):
+            print(" ",end=" ")
+    print()
+print()
