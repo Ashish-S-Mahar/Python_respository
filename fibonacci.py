4f7100a8a323ecfa193716e9289ba058e93b5dfb
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-02 13:21:05 +0530

    Adding codes in my file.

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,21 @@
 # Its a "Fibonacci series" made by "if,else"condition...
+
+x = 0
+y = 1
+z = int(input("Enter the value..."))
+
+if(z==0):
+    print("Enter value...")
+elif(z==1):
+    print(x)
+elif(z==2):
+    print(x)
+    print(y)
+else:
+    print(x)
+    print(y)
+    for i in range(1,z-1):
+        a = x+y
+        x=y
+        y=a
+        print(a) 
