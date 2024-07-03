f8eaf865c2c1cefaddb5aa0aea24eb417d32baae
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-03 22:12:38 +0530

    Adding a multiline string

--- PythonTution/strings.py
+++ PythonTution/strings.py
@@ -1,16 +1,3 @@
-# x = """Hi its a         #multiline string
-# multiline srting"""
-# y = "Its a single line string"
-# print(x)
-# print(y)
-
-# # for printing ascii values
-# z = 2
-# print(ord("z"))
-# print(ord("a"))
-# print(ord(" "))
-
-# # changing ascii to normal
-# print(chr(z))
-# print(chr(97))
-
+x = """Hi its a         # multiline strings
+multiline srting"""
+print(x)
