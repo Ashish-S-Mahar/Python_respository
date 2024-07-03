bb8909368bcae4622955031853a6ee01f5b3c4e7
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-03 22:24:43 +0530

    Adding the codes to convert in normal value

--- PythonTution/Ascii.py
+++ PythonTution/Ascii.py
@@ -1,7 +1,13 @@
 # for printing ascii values
 
+
 # Changing normal values to ascii values
 z = 2
 print(ord("z"))
 print(ord("a"))
 print(ord(" "))
+
+
+# changing ascii to normal
+print(chr(z))
+print(chr(97))      #value of "a"
