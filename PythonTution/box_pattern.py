8adf372928bcbe720b3ef0ea3a1e804a7c3d0146
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-03 22:32:45 +0530

    Adding codes in my file.

--- PythonTution/box_pattern.py
+++ PythonTution/box_pattern.py
@@ -1,1 +1,8 @@
 # Its a code of making a box pattern by python with the help of loops......
+
+x = int(input("Enter the value:- "))        # getting limit by user.....
+
+for i in range(1,x+1):
+    for j in range(1,x+1):
+        print("x",end="  ")
+    print()
