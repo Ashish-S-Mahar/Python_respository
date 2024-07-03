4d46c0b21120ee4eb9c23c482fac8ed89aad6907
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-03 16:13:08 +0530

    Adding new file

---
+++ PythonTution/strings.py
@@ -0,0 +1,16 @@
+# x = """Hi its a         #multiline string
+# multiline srting"""
+# y = "Its a single line string"
+# print(x)
+# print(y)
+
+# # for printing ascii values
+# z = 2
+# print(ord("z"))
+# print(ord("a"))
+# print(ord(" "))
+
+# # changing ascii to normal
+# print(chr(z))
+# print(chr(97))
+
