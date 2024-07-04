67b0cb83bb9b52da34dd29119a84f04585a01a80
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-04 21:49:18 +0530

    Adding codes in my file

--- PythonTution/vowelcounter.py
+++ PythonTution/vowelcounter.py
@@ -1,2 +1,13 @@
 # To count vowels in file....
 
+x = input("Enter the name....")
+x = "ashish"
+y = len(x)
+print(y)
+count = 0
+for i in x:
+    if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u"):
+        count+=1
+        print(i)
+    else:
+        print("",end="")
