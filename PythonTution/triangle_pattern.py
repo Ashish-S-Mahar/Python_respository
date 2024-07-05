67d5166f7b48b5d0bffc97492cbec89cbf0bd3ee
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-05 22:30:59 +0530

    Adding comment for ak detailed information....

--- PythonTution/triangle_pattern.py
+++ PythonTution/triangle_pattern.py
@@ -2,3 +2,9 @@
 
 x = int(input("Enter the value:- "))
 
+for i in range(1,x+1):
+    for j in range(1,i+1):
+        print("x",end=" ") 
+    print()
+
+# here i loop is used for changing lines and j loop is used for printing x or stars.....
