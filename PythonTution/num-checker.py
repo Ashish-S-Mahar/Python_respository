75c8293ba29415d060e3e7307ea10331b8985418
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-05 19:46:43 +0530

    Adding codes to my file.

--- PythonTution/num-checker.py
+++ PythonTution/num-checker.py
@@ -1,3 +1,15 @@
 # Its a number checker code which checks the num is negative or positive  
 
 x = int(input("enter number:"))
+
+
+x = int(input("enter number:"))
+
+if(x<0):
+    print("The number is negative")
+
+elif(x==0):
+    print("number is zero")
+
+else:
+    print("number is positive")
