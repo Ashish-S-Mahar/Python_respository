a3e41bb9a306bd9922fbc74cd4bfb2d4a4b93351
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-06 22:18:44 +0530

    Adding codes to my file

--- PythonTution/pass_cheker.py
+++ PythonTution/pass_cheker.py
@@ -1,1 +1,13 @@
 #  Its a code of checking the password........
+
+
+
+x = "Ashish"
+
+y = input("Enter your password:-")
+
+if(x==y):
+    print("Your password is correct")
+
+else:
+    print(" try again later")
