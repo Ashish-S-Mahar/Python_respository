f821ec908ad9f3fa61425fddb93ec9ac6eeda906
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-06 22:08:50 +0530

    Adding codes to my file

--- PythonTution/result.py
+++ PythonTution/result.py
@@ -1,1 +1,10 @@
 # Its a code in which you check that you is pass or fail 
+
+
+x = int(input("inter your marks:"))
+
+if(x>=33):
+    print("you is passed")
+
+else:
+    print("You is fail. Better luck next time")
