01a0b96e5f7cd5e67eb1b5b450b3c8726b502853
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-07 20:31:59 +0530

    Addingg codes to my files'

--- PythonTution/table_of_4.py
+++ PythonTution/table_of_4.py
@@ -1,2 +1,6 @@
-# Its a table of 3 which is developed by the simple codes....
+# Its a table of 4 which is developed by the simple codes....
+
+
+for i in range(1,11):
+    print("4 x",i,"=" ,4*i)
 
