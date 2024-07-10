8df56a0a90a3e487aaa3e0d208202c76f8df968c
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-10 21:35:09 +0530

    adding info i9n my file

--- PythonTution/taable_of_10.py
+++ PythonTution/taable_of_10.py
@@ -1,6 +1,14 @@
-# Its a table of 9 which is developed by the simple codes....
+# Its a table of 10 which is developed by the simple codes....
 
 
 
 for i in range(1,11):
-    print("9 x",i,"=" ,9*i)
+    print("10 x",i,"=" ,10*i)
+
+
+
+
+# it prints like 
+# 9 x 1 = 9
+# 9 x 2 = 18
+
