a5db903539a05e5baaca8aaaf8b803e7327bd62c
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-13 20:12:45 +0530

    adding a comment in file

--- PythonTution/area_of_circle.py
+++ PythonTution/area_of_circle.py
@@ -4,3 +4,4 @@
 x = int(input("Enter the radius of circle:"))
 print("Area of circle is", 3.14159*x*x)
 
+# to find the area of triangle we have to enter the radius of the circle.......................
