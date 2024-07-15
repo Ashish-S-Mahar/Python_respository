1914c28ef0f127b9a57f438475e1427142994ebe
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-15 20:33:57 +0530

    adding codes

--- list1.py
+++ list1.py
@@ -3,3 +3,11 @@ list = [12,"hii",4.2,0] #creating list
 print(list)
 
 # Creating list by user 
+
+y = int(input("Enter the length..."))  
+z = []
+for i in range(1,y+1):
+    x = int(input("enter the value    "))
+    z += [x]
+print(z)
+ṇ
