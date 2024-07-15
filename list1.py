5107bc51177f6cc658f3d8981585283e406a8ae2
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-15 20:46:35 +0530

    Improving file

--- list1.py
+++ list1.py
@@ -1,13 +1,24 @@
 # in this file we create a list
-list = [12,"hii",4.2,0] #creating list
+
+
+
+list = [12,"hii",4.2,0]                     #creating list
+
 print(list)
+
 
 # Creating list by user 
 
-y = int(input("Enter the length..."))   # asking length from user
-z = []  # creating empty list
-for i in range(1,y+1):  # starting a loop 
+
+
+y = int(input("Enter the length..."))       #asking length from user
+
+z = []                                      # creating empty list
+
+for i in range(1,y+1):                      # starting a loop 
+
     x = int(input("enter the value    "))   # asking element of lists 
-    z += [x]    # adding element in empty list 
+
+    z += [x]                                # adding element in empty list 
+
 print(z)
-ṇ
