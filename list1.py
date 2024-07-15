277ca1dfca68db3ab1fd54ac2f3641ce57016c2b
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-15 20:36:03 +0530

    Adding comment

--- list1.py
+++ list1.py
@@ -4,10 +4,10 @@ print(list)
 
 # Creating list by user 
 
-y = int(input("Enter the length..."))  
-z = []
-for i in range(1,y+1):
-    x = int(input("enter the value    "))
-    z += [x]
+y = int(input("Enter the length..."))   # asking length from user
+z = []  # creating empty list
+for i in range(1,y+1):  # starting a loop 
+    x = int(input("enter the value    "))   # asking element of lists 
+    z += [x]    # adding element in empty list 
 print(z)
 ṇ
