5ae64ae670f7ef034d97cb92b0ee60dc7d7fce67
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-18 20:50:27 +0530

    Adding rupee value

--- money.py
+++ money.py
@@ -20,3 +20,24 @@ rs200 = 0           # For adding total money....
 rs500 = 0           # For adding total money.... 
 
 rs2000 = 0          # For adding total money.... 
+
+
+one = 1                     
+
+two = 2                     
+
+five = 5                    
+
+ten = 10                    
+
+twenty = 20                 
+
+fifty = 50                  
+
+hundred = 100               
+
+t_hun = 200                 
+
+f_hun = 500                 
+
+t_thous = 2000              
