9d7966007ebfc8aa3286d5b1b7aea0b2ebecb978
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-18 20:47:44 +0530

    adding 0 value

--- money.py
+++ money.py
@@ -1,3 +1,23 @@
 #  Its a file in which we count change money.....
 
 x = int(input("Enter total Amount :-     "))
+
+rs1 = 0             
+
+rs2 = 0             
+
+rs5 = 0             
+
+rs10 = 0            
+
+rs20 = 0            
+
+rs50 = 0            
+
+rs100 = 0           
+
+rs200 = 0           
+
+rs500 = 0           
+
+rs2000 = 0          
