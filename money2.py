ce12e8123e2beb729873943c718d64eea4c0f61e
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-19 19:58:18 +0530

    Adding a 4 more if  in my  file

--- money2.py
+++ money2.py
@@ -19,3 +19,18 @@ count1 = 0
 if(two000<= x):
     x = x // two000
     count2000 += x%2000
+if(five00<= x):
+    x = x // five00
+    count500 += x%500 
+
+if(two00 <= x):
+    x = x // two00
+    count200 += x%200
+
+if(hun <= x):
+    x = x // hun
+    count100 += x % 100
+
+if(fifty <= x):
+    x = x // fifty
+    count50 += x%50
