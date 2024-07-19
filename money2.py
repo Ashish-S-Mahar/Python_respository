190e0b399ca0804e3b05ce8d99cfcba2bc5de43f
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-19 19:55:57 +0530

    Adding empty variable in my  file

--- money2.py
+++ money2.py
@@ -1,3 +1,15 @@
 #  chutta dhoondo
 
 x = int(input("Enter the amount :- "))
+
+
+count2000 = 0
+count500 = 0
+count200 = 0
+count100 = 0
+count50 = 0
+count20 = 0 
+count10 = 0
+count5 = 0
+count2 = 0
+count1 = 0
