7edb4bf07a3c13696db1853ac4de9e620e934724
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-20 19:37:01 +0530

    Adding main codes

--- greaterlist.py
+++ greaterlist.py
@@ -4,3 +4,11 @@ list = [1,4,2,6,8,10]
 
 list.append(13)
 list.append(11)
+
+def func(list):
+    lar = list[0]
+    for li in list:
+        if(li>lar):
+            lar = li
+    print(lar)
+func(list)
