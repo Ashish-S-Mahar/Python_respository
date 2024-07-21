2e91dc3ae48ae8bf1e1ae351736faf0dbaaca6f0
Ashish-S-Mahar <asmaharg1139@gmail.com>
2024-07-21 18:57:08 +0530

    Adding new codes

--- PythonTution/reversinglist.py
+++ PythonTution/reversinglist.py
@@ -1,2 +1,16 @@
 #  Reversing a list without using reverse
 
+
+x = ["hii", "hello", "hey","why",2," ",]
+# x = len(x)
+print(x)
+y = []
+count = 0
+for i in range(len(x)-1,-1,-1):
+
+    y.append(x[i])
+    
+print(y)
+
+
+
